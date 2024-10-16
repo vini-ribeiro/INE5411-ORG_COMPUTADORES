754fa2e1dbebc7f233fde15396d8ffe7968071e2
Vinicius Henrique Ribeiro <vini.ribeiro2003@gmail.com>
2024-10-15 22:45:27 -0300

    TODO: passar para double

--- programas_mips/lab03/exe02.asm
+++ programas_mips/lab03/exe02.asm
@@ -1,32 +1,63 @@
 .data
-
+	
 .text
+main:
+	li	$a0, 10
+	li	$a1, 0
+	jal	potencia_n
+	j	exit_programa
 
 
+seno_x:
+	move	$s0, $a0	# salva x em s0
+	li	$t7, 1		# configura uma variável para alterar o sinal da operação dos monômios
+	li	$t0, 1		# inicializa t0 (expoente e fatorial)
+loop_seno:
+	bgt	$t0, 41, exit_loop_seno
+	move	$a0, $s0	# x é passado como base no argumento de potencia_n
+	move	$a1, $t0	# éxpoente da potencia_n
+	jal	potencia_n	# chamada da função potencia_n
+	move	$t1, $v0	# pega o retorno de potencia_n
+	move	$a0, $t0	# passa o argumento para fatorial
+	jal	fatorial
+	move	$t2, $v0	# salva em t2 o resultado de fatorial
+	div	$t3, $t1, $t2	# faz x^n/n!
+	mul	$t3, $t3, $t7	# configura o sinal do monômio
+	sub	$t7, $zero, $t7	# inverte o sinal para a próxima iteração
+	add	$t4, $t4, $t3	# soma o monômio no acumulador
+	addi	$t0, $t0, 2	# incrementa o expoente e fatorial para a próxima iteração
+	j	loop_seno	
+exit_loop_seno:
+	move	$v0, $t4
+	jr	$ra
+	
+	
 
-potencia_n:	# procedimento para calcular a n-ésima potência
-	move	$t0, $a0	# pega a base
-	move	$t1, $a1	# pega o expoente
-	li	$t2, 1
+
+# procedimento para calcular a n-ésima potência (a0, a1) = (base, expoente)
+potencia_n:	
+	li	$v0, 1		# configura o retorno
 loop_pot:
-	beq	$t1, $zero, exit_loop_pot
-	addi	$t1, $t1, -1
-	mul	$t2, $t2, $t0
+	beqz	$a1, exit_loop_pot
+	addi	$a1, $a1, -1	# decrementa o expoente
+	mul	$v0, $v0, $a0	# acrescente no acumulador a parcela da potência
 	j	loop_pot
 exit_loop_pot:
-	move	$v0, $t2
-	jr	$ra
+	jr	$ra	# fim do procedimento potencia_n
 
 
-f_fatorial:	# fatorial de um número dado
-	move 	$t0, $a0	# t0 guarda o argumento
-	li	$t1, 1		# contador do loop
-	move	$t2, $a0	# acumulador do multiplicador
+
+# fatorial de um número dado
+fatorial:
+	li	$v0, 1		# contador do loop
 loop_fat:
-	beq	$t0, $t1, exit_loop_fat	# t0 vai decrementando até chegar em 1
-	addi	$t0, $t0, -1
-	mul	$t2, $t2, $t0		# multiplica o valor de t0 pelo acumulado guardado em t2
+	ble	$a0, 1, exit_loop_fat	# t0 vai decrementando até chegar em 1
+	mul	$v0, $v0, $a0		# acumulador para as multiplicações
+	addi	$a0, $a0, -1		# decrementa a0 para achar o próximo multiplicador
 	j	loop_fat
 exit_loop_fat:
-	move 	$v0, $t2	# bota o acumulado em v0 para retorno
-	jr	$ra	# volta para a função chamada
+	jr	$ra	# fim do procedimento do fatorial
+
+
+
+exit_programa:	# label para quando a main terminar (fim do programa)
